4228c1f7330c44415c85f648f3d33ab0266603c1
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-08-20 22:29:17 +0700

    integrate carosel into section

--- src/customer/component/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/component/HomeSectionCard/HomeSectionCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomeSectionCard = () => {
   return (
     <div className='cursor-pointer flex flex-col items-center bg-white 
-    rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3'>
+    rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border'>
 
         <div className='h-[13rem] w-[10rem]'>
             <img className='object-cover object-top w-full h-full' 
--- src/customer/component/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/component/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -11,19 +11,47 @@ const HomeSectionCarosel = () => {
         1024: { items: 5.5 },
     };
 
-    const items = [1,1,1,1,1].map((item)=><HomeSectionCard/>)
+    const items = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1].map((item)=><HomeSectionCard/>)
   return (
-    <div className='relative px-4 lg:px-8'>
+    <div className='border'>
         <div className='relative p-5'>
             <AliceCarousel
             items={items}
             disableButtonsControls  //tắt điều khiển nút điều hướng
             infinite                //chuyển đổi vô hạn
             responsive={responsive}
+            disableDotsControls
         />
-        <Button variant='contained' className='z-50' sx={{position:'absolute', 
-        top:'8rem', right:"0rem", transform:"translateX(50%) rotate(90deg)", bgcolor:"white"}} aria-label='next'>
-            <ArrowLeftIcon sx={{transform:"rotate(90deg)", color:"black"}}/>
+        <Button 
+        variant='contained' 
+        className='z-50' 
+        sx={{
+            position:'absolute', 
+            top:'8rem', 
+            right:"0rem", 
+            transform:"translateX(50%) rotate(90deg)", 
+            bgcolor:"white"}} 
+            aria-label='next'>
+            <ArrowLeftIcon 
+            sx={{transform:"rotate(90deg)", color:"black"}}/>
+        </Button>
+
+        <Button 
+        variant='contained' 
+        className='z-50' 
+        sx={{
+            position:'absolute', 
+            top:'8rem', 
+            left:"0rem", 
+            transform:"translateX(-50%) rotate(-90deg)",
+            bgcolor:"white"
+            }} 
+            aria-label='next'>
+            <ArrowLeftIcon 
+            sx={{
+                transform:"rotate(90deg)", 
+                color:"black"
+                }}/>
         </Button>
         </div>
     </div>
