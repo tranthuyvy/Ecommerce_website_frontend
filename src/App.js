82d7828ffa25b7914e99ca402766a0ae3fa5dae2
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-08-20 09:52:59 +0700

    integrate carosel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./customer/component/Navigation/Navigation";
+import HomePage from "./customer/pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <div>Home</div>
+      <div>
+        <HomePage />
+      </div>
     </div>
   );
 }
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarosel from '../../component/HomeCarosel/MainCarosel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarosel/>
+        <div>
+            orther
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
