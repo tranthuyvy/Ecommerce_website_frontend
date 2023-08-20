43f7ad56f94d2289539a8ab05ccf3b6e216aaed1
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-08-20 19:31:18 +0700

    add ArrowLeftIcon

--- src/customer/component/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/component/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel';
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
+import { Button } from '@mui/material';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 
 const HomeSectionCarosel = () => {
     const responsive = {
@@ -19,6 +21,10 @@ const HomeSectionCarosel = () => {
             infinite                //chuyển đổi vô hạn
             responsive={responsive}
         />
+        <Button variant='contained' className='z-50' sx={{position:'absolute', 
+        top:'8rem', right:"0rem", transform:"translateX(50%) rotate(90deg)", bgcolor:"white"}} aria-label='next'>
+            <ArrowLeftIcon sx={{transform:"rotate(90deg)", color:"black"}}/>
+        </Button>
         </div>
     </div>
   )
