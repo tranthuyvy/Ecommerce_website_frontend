d08d4914312410e6057345110968270693dd148f
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-08-23 01:19:51 +0700

    basic about ProductDetail

--- src/customer/component/ProductDetail/ProductDetail.jsx
+++ src/customer/component/ProductDetail/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/20/solid";
 import { RadioGroup } from "@headlessui/react";
-import { Rating } from "@mui/material";
+import { Button, Rating } from "@mui/material";
 
 const product = {
   name: "Basic Tee 6-Pack",
@@ -34,15 +34,9 @@ const product = {
     { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
     { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
   ],
-  sizes: [
-    { name: "XXS", inStock: false },
-    { name: "XS", inStock: true },
-    { name: "S", inStock: true },
-    { name: "M", inStock: true },
-    { name: "L", inStock: true },
-    { name: "XL", inStock: true },
-    { name: "2XL", inStock: true },
-    { name: "3XL", inStock: true },
+  author: [
+    { name: "T", inStock: false },
+    { name: "V", inStock: true },
   ],
   description:
     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
@@ -63,7 +57,7 @@ function classNames(...classes) {
 
 export default function ProductDetail() {
   const [selectedColor, setSelectedColor] = useState(product.colors[0]);
-  const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+  const [selectedAuthor, setSelectedAuthor] = useState(product.author[2]);
 
   return (
     <div className="bg-white">
@@ -138,6 +132,9 @@ export default function ProductDetail() {
               <h1 className="text-lg lg:text-x1 text-gray-900 opacity-60 pt-1">
                 Kỹ Năng Sống
               </h1>
+              <h5 className="text-lg lg:text-x1 font-semibold text-gray-900">
+                Author Name
+              </h5>
             </div>
 
             {/* Options */}
@@ -160,77 +157,32 @@ export default function ProductDetail() {
               </div>
 
               <form className="mt-10">
-                {/* Colors */}
-                <div>
-                  <h3 className="text-sm font-medium text-gray-900">Color</h3>
+                 
 
-                  <RadioGroup
-                    value={selectedColor}
-                    onChange={setSelectedColor}
-                    className="mt-4"
-                  >
-                    <RadioGroup.Label className="sr-only">
-                      Choose a color
-                    </RadioGroup.Label>
-                    <div className="flex items-center space-x-3">
-                      {product.colors.map((color) => (
-                        <RadioGroup.Option
-                          key={color.name}
-                          value={color}
-                          className={({ active, checked }) =>
-                            classNames(
-                              color.selectedClass,
-                              active && checked ? "ring ring-offset-1" : "",
-                              !active && checked ? "ring-2" : "",
-                              "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none"
-                            )
-                          }
-                        >
-                          <RadioGroup.Label as="span" className="sr-only">
-                            {color.name}
-                          </RadioGroup.Label>
-                          <span
-                            aria-hidden="true"
-                            className={classNames(
-                              color.class,
-                              "h-8 w-8 rounded-full border border-black border-opacity-10"
-                            )}
-                          />
-                        </RadioGroup.Option>
-                      ))}
-                    </div>
-                  </RadioGroup>
-                </div>
-
-                {/* Sizes */}
-                <div className="mt-10">
+                {/* Author */}
+                {/* <div className="mt-10">
                   <div className="flex items-center justify-between">
-                    <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                    <a
-                      href="#"
-                      className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Size guide
-                    </a>
-                  </div>
+                    <h3 className="text-sm font-medium text-gray-900">Author</h3>
+                    
+                  </div> */}
 
-                  <RadioGroup
-                    value={selectedSize}
-                    onChange={setSelectedSize}
+                  {/* <RadioGroup
+                    value={selectedAuthor}
+                    onChange={setSelectedAuthor}
                     className="mt-4"
                   >
                     <RadioGroup.Label className="sr-only">
-                      Choose a size
+                      Choose a author
                     </RadioGroup.Label>
                     <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                      {product.sizes.map((size) => (
+                      {product.author.map((author) => (
                         <RadioGroup.Option
-                          key={size.name}
-                          value={size}
-                          disabled={!size.inStock}
+                          key={author.name}
+                          value={author}
+                          disabled={!author.inStock}
                           className={({ active }) =>
                             classNames(
-                              size.inStock
+                                author.inStock
                                 ? "cursor-pointer bg-white text-gray-900 shadow-sm"
                                 : "cursor-not-allowed bg-gray-50 text-gray-200",
                               active ? "ring-2 ring-indigo-500" : "",
@@ -241,9 +193,9 @@ export default function ProductDetail() {
                           {({ active, checked }) => (
                             <>
                               <RadioGroup.Label as="span">
-                                {size.name}
+                                {author.name}
                               </RadioGroup.Label>
-                              {size.inStock ? (
+                              {author.inStock ? (
                                 <span
                                   className={classNames(
                                     active ? "border" : "border-2",
@@ -280,15 +232,12 @@ export default function ProductDetail() {
                         </RadioGroup.Option>
                       ))}
                     </div>
-                  </RadioGroup>
-                </div>
+                  </RadioGroup> */}
+                {/* </div> */}
 
-                <button
-                  type="submit"
-                  className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Add to bag
-                </button>
+                <Button variant="contained" sx={{px:"2rem",py:"1rem", bgcolor:"#9155fd"}}>
+                  Add To Cart
+                </Button>
               </form>
             </div>
 
