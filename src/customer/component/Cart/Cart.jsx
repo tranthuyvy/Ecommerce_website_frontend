e3ee47849eb9573c2332bf14c7edc33e2248e9fd
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-08-23 23:21:36 +0700

    cart page

--- src/customer/component/Cart/Cart.jsx
+++ src/customer/component/Cart/Cart.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import CartItem from "./CartItem";
+import { Button } from "@mui/material";
 
 const Cart = () => {
   return (
     <div>
       <div className="lg:grid grid-cols-3 lg:px-16 relative">
         <div className="col-span-2">
-          <CartItem />
+          {[1,1,1].map((item)=> <CartItem/>)}
         </div>
         <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0">
-          <div className="border">
+          <div className="border px-5 py-5">
             <p className="uppercase font-bold opacity-60 pb-4">Price details</p>
             <hr />
-            <div className="space-y-3 font-semibold">
+            <div className="space-y-3 font-semibold mb-10">
               <div className="flex justify-between pt-3 text-black">
                 <span>Price</span>
                 <span>327.000</span>
@@ -30,6 +31,9 @@ const Cart = () => {
                 <span className="text-green-600">297.000</span>
               </div>
             </div>
+            <Button variant="contained" className="w-full mt-5" sx={{px:"2.5rem",py:".7rem", bgcolor:"#9155fd"}}>
+              Checkout
+            </Button>
           </div>
         </div>
       </div>
