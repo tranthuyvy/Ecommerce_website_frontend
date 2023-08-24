93d98e62f6385b20fe0327c44321c5ab5ed7f138
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-08-24 09:53:09 +0700

    delivery Address Form

--- src/customer/component/Checkout/Checkout.jsx
+++ src/customer/component/Checkout/Checkout.jsx
@@ -9,17 +9,12 @@ import { useLocation } from "react-router-dom";
 import DeliveryAddressForm from "./DeliveryAddressForm";
 import OrderSummary from "./OrderSummary";
 
-const steps = [
-  "Login",
-  "Delevery Address",
-  "Order Summary",
-  "Payment",
-];
+const steps = ["Login", "Delivery Address", "Order Summary", "Payment"];
 
 export default function Checkout() {
   const [activeStep, setActiveStep] = React.useState(0);
   const location = useLocation();
-  const querySearch= new URLSearchParams(location.search)
+  const querySearch = new URLSearchParams(location.search);
 
   const step = querySearch.get("step");
 
@@ -33,47 +28,42 @@ export default function Checkout() {
 
   return (
     <div className="px-10 lg:px-20">
-        <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={step}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
+      <Box sx={{ width: "100%" }}>
+        <Stepper activeStep={step}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
 
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+            </Box>
 
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-
-          </Box>
-          <div>
-            {step == 2 ? <DeliveryAddressForm/>:<OrderSummary/>}
-          </div>
-        </React.Fragment>
-      )}
-    </Box>
+            <div>{step == 2 ? <DeliveryAddressForm /> : <OrderSummary />}</div>
+          </React.Fragment>
+        )}
+      </Box>
     </div>
-    
   );
 }
